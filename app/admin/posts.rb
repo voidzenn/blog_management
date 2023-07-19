947a2e03ed45047d1839477adbbda0d0aab69e61
mike <mikesil121@gmail.com>
2023-07-19 16:32:34 +0800

    Added the action button in Post new page

--- app/admin/posts.rb
+++ app/admin/posts.rb
@@ -1,18 +1,12 @@
 ActiveAdmin.register Post do
+  permit_params :title, :content, :author_id
 
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # Uncomment all parameters which should be permitted for assignment
-  #
-  # permit_params :author_id, :title, :content, :status
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:author_id, :title, :content, :status]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
-  
+  form do |f|
+    f.inputs do
+      f.input :author_id, as: :select, collection: AdminUser.pluck(:name, :id)
+      f.input :title
+      f.input :content
+    end
+    f.actions
+  end
 end
