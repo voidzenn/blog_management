3050e930355dbd48a642964c9b5ed2f0c7d85991
mike <mikesil121@gmail.com>
2023-07-20 17:12:11 +0800

    Modified the post new form to have quill_editor and modified index

--- app/admin/posts.rb
+++ app/admin/posts.rb
@@ -1,12 +1,39 @@
 ActiveAdmin.register Post do
   permit_params :title, :content, :author_id
 
+  index do
+    selectable_column
+    id_column
+    column :author.name.capitalize
+    column :title
+    column :status
+    column :created_at
+    actions
+  end
+
   form do |f|
     f.inputs do
       f.input :author_id, as: :select, collection: AdminUser.pluck(:name, :id)
       f.input :title
-      f.input :content, as: :quill_editor
+      f.input :content, as: :quill_editor, input_html: {
+        data: {
+          options: {
+            modules: {
+              toolbar:
+                [
+                  [{"header": [1, 2, 3, 4, false]}],
+                  ["bold", "italic", "underline"],
+                  ['blockquote', 'code-block'],
+                  ["link", "image"],
+                ]
+              },
+            placeholder: "Type something...",
+            theme: "snow",
+          }
+        }
+      }
     end
     f.actions
   end
+
 end
