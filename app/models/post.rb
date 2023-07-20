2dfaa9bcf5cb5eb3af814329528b851f9757edf2
mike <mikesil121@gmail.com>
2023-07-20 17:12:57 +0800

    Added validation for post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,10 @@
 class Post < ApplicationRecord
   belongs_to :author, class_name: "AdminUser", foreign_key: "author_id"
 
+  validates :title, presence: true
+  validates :content, presence: true
+  validates :author_id, presence: true
+
   def self.ransackable_attributes(auth_object = nil)
     ["author_id", "content", "created_at", "id", "status", "title", "updated_at"]
   end
