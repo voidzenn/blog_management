92f85d3c13a5b8877e5a41107a30bbeede7d1279
mike <mikesil121@gmail.com>
2023-07-21 15:03:16 +0800

    Fixed AddNameColumnInAdminUser migration

--- db/migrate/20230719082137_add_name_column_in_admin_user.rb
+++ db/migrate/20230719082137_add_name_column_in_admin_user.rb
@@ -1,5 +1,9 @@
 class AddNameColumnInAdminUser < ActiveRecord::Migration[7.0]
-  def change
+  def up
     add_column :admin_users, :name, :string, after: :encrypted_password
+  end
+
+  def down
+    remove_column :admin_users, :name, :string, after: :encrypted_password
   end
 end
