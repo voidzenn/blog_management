7e877b4bd6981d830da0b313d3894ce59e19930c
mike <mikesil121@gmail.com>
2023-07-21 15:04:11 +0800

    Fxied AddCategoryToPosts migration

--- db/migrate/20230721065539_add_category_to_posts.rb
+++ db/migrate/20230721065539_add_category_to_posts.rb
@@ -1,9 +1,9 @@
 class AddCategoryToPosts < ActiveRecord::Migration[7.0]
   def up
-    add_column :posts, :category, :integer, foreign_key: true
+    add_column :posts, :category, :integer, foreign_key: true, after: :title
   end
 
   def down
-    remove_column :posts, :category, :integer, foreign_key: true
+    remove_column :posts, :category, :integer, foreign_key: true, after: :title
   end
 end
