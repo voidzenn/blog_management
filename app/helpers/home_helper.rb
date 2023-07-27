d0b8e6e48719002c8166992c1840cc819edf8976
mike <mikesil121@gmail.com>
2023-07-27 11:09:48 +0800

    Added titlte to home posts card and modified helper function

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,11 +1,9 @@
 module HomeHelper
   def shortened_safe_content content, length: 400
-    sanitize_content(truncate(strip_tags(content), length: length, separator: ""))
+    sanitize_data(truncate(strip_tags(content), length: length, separator: ""))
   end
 
-  private
-
-  def sanitize_content content
-    sanitize(content, tags: %w(p h1 h2 h3 strong em a ul ol li))
+  def sanitize_data data
+    sanitize(data, tags: %w(p h1 h2 h3 strong em a ul ol li))
   end
 end
