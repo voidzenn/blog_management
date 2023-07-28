b3f46f67ae26ceeebce53665b4dc6d2ec509e8a5
mike <mikesil121@gmail.com>
2023-07-28 14:41:09 +0800

    Modified posts factory to have move content length

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :post do
     title { Faker::Lorem.sentence }
-    content { Faker::Lorem.sentences(number: 30).join(" ") }
+    content { Faker::Lorem.sentences(number: 150).join(" ") }
     author { create :admin_user }
     category { create :category }
   end
