63c7c8fa77e1ade09da9336565a7a3222e13990b
Mike <mikesil121@gmail.com>
2024-09-08 19:34:33 +0800

    [Feature][Advance-Search]

--- db/migrate/20230720091715_change_post_content_to_longtext.rb
+++ db/migrate/20230720091715_change_post_content_to_longtext.rb
@@ -1,6 +1,6 @@
 class ChangePostContentToLongtext < ActiveRecord::Migration[7.0]
   def up
-    change_column :posts, :content, :longtext
+    change_column :posts, :content, :text
   end
 
   def down
--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :post do
-    title { Faker::Lorem.sentence }
+    title { Faker::Marketing.unique.buzzwords.capitalize }
     content { Faker::Lorem.sentences(number: 150).join(" ") }
     author { create :admin_user }
     category { Category.all.sample }
